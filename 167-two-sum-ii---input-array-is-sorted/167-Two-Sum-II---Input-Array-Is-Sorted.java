8a135e3293e3b9c452be19359ed2894af5e2db5c
Harsh <131451137+HarshStacks@users.noreply.github.com>
2026-07-03 14:31:00 +0530

    [167] [Time Beats: 95.93%] [Memory Beats: 42.08%] - LeetPush

--- 167-two-sum-ii---input-array-is-sorted/167-Two-Sum-II---Input-Array-Is-Sorted.java
+++ 167-two-sum-ii---input-array-is-sorted/167-Two-Sum-II---Input-Array-Is-Sorted.java
@@ -1,18 +1,19 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        
-        Map<Integer,Integer> indicies = new HashMap<>();
+        int i = 0;
+        int j = numbers.length-1;
 
-        for(int i=0; i<numbers.length; i++){
-            indicies.put(numbers[i],i);
-        }
+        while(i<j){
+            int sum = numbers[i] + numbers[j]; 
+            if(sum == target)
+                return new int[]{i+1,j+1};
 
-        for(int i=0; i<numbers.length; i++){
-            int diff = target - numbers[i];
-            if(indicies.containsKey(diff) && indicies.get(diff) != i){
-                return new int[]{i+1,indicies.get(diff)+1};
-            } 
-        }
-        return new int[]{-1, -1};
+            else if(sum>target)
+                j--;
+
+            else
+                i++;
+        } 
+        return new int[]{-1,-1};
     }
 }
